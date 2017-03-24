76d5d36cfc66dd570057c34f62595cf9044af0d6
domcodes <email@domcodes.com>
2017-03-24 17:34:04 +0000

    Set up project

---
+++ script.js
@@ -0,0 +1,22 @@
+//1. Create a variable and capture input
+
+
+
+
+//2. Create a separate variable for each piece of input
+
+
+
+//3. Add alert to tell user they are finished
+
+
+
+
+//4. Combine input with other strings to create message
+
+
+
+
+
+
+//5. print story to the browser window
