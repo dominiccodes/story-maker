a82999e36521d490003df0729bb71660c2b319e4
domcodes <email@domcodes.com>
2017-03-24 17:41:56 +0000

    Step 1 and test

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //1. Create a variable and capture input
 
-
-
+var adjective = prompt('Please type an adjective');
+var sentence = "<h2>There once was a " + adjective;
+document.write(sentence);
 
 //2. Create a separate variable for each piece of input
 
@@ -14,8 +15,8 @@
 
 //4. Combine input with other strings to create message
 
-
-
+//for reference
+"<h2>There once was a [adjective] coder who wanted to use JavaScript to [verb] the [noun].</h2>"
 
 
 
