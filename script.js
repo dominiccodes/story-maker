5103c4dd85ebe422fa8c5cf558523ef9eba694b9
domcodes <email@domcodes.com>
2017-03-24 17:47:42 +0000

    Add rest of prompts and alert

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
+//Instructions
 //1. Create a variable and capture input
-
+//2. Create a separate variable for each piece of input
+//3. Add alert to tell user they are finished
+//4. Combine input with other strings to create message
+//5. print story to the browser window
 var adjective = prompt('Please type an adjective');
 var sentence = "<h2>There once was a " + adjective;
+var verb = prompt('Please type a verb');
+var noun = prompt('Please type a noun');
+
+alert('All done, ready for the story?');
+
+
 document.write(sentence);
 
-//2. Create a separate variable for each piece of input
-
-
-
-//3. Add alert to tell user they are finished
 
 
 
 
-//4. Combine input with other strings to create message
+
+
 
 //for reference
 "<h2>There once was a [adjective] coder who wanted to use JavaScript to [verb] the [noun].</h2>"
 
 
-
-//5. print story to the browser window
